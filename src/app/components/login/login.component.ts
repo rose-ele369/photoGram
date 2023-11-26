720498fb0a50e9a13b8a7b89e072b8c879ee2442
Rosa Vallejo <tu_correo_electronico>
2023-11-26 21:03:26 +0100

    Diseño incial del login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+  username: string = '';
+  password: string = '';
 }
